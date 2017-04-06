fded86ae4654803c2b72ec34a3d0b1e7235f1777
Corey Burns <coreyaburns@gmail.com>
2017-04-06 10:59:00 -0500

    Service worker up and running!

--- sw.js
+++ sw.js
@@ -1,14 +1,17 @@
+const CACHE_NAME = 'sw-test-cache-v1';
+const urlsToCache = [
+  '/',
+  '/dist/css/styles.min.css',
+  '/assets/img/moss-typing.gif',
+  'https://fonts.googleapis.com/css?family=Roboto'
+]
+
 self.addEventListener('install', event => {
   console.log('SW v1 installing...');
 
   event.waitUntil(
-    caches.open('sitecache-v1').then( cache => {
-      cache.addAll([
-        '/index.html',
-        '/dist/css/styles.min.css',
-        'https://fonts.googleapis.com/css?family=Roboto',
-        '/assets/img/moss-typing.gif'
-      ])
+    caches.open(CACHE_NAME).then( cache => {
+        cache.addAll(urlsToCache);
     })
   );
 });
@@ -19,30 +22,15 @@ self.addEventListener('activate', event => {
 });
 
 
-self.addEventListener('fetch', event => {
-  console.log('handling fetch event for: ', event.request.url);
-
-  event.respondWith( // takes a promise or response obj
-      caches.match(event.request).then( response => {
-        if(response) {
-          console.log('Found response in cache', response);
-
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then( response => {
+        // Cache hit - return response
+        if (response) {
           return response;
         }
-        console.log('No response found in cache...fetching from network')
-
-        return fetch(event.request).then( response => {
-          console.log('Response from network: ', response);
-
-          return response;
-        }).catch( error => {
-          console.error('FETCHING FAILED: ', error);
-
-          throw error;
-        })
+        return fetch(event.request);
       })
-      // caches.open('sitecache-v1').then( cache => {
-      //   return cache.match(event.request);
-      // })
-  )
+  );
 });
